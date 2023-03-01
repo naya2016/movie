617ce816c18e8e01a71b02a505e89a81491f3f52
naya2016 <63491927+naya2016@users.noreply.github.com>
2023-03-01 10:41:31 +0000

    de

--- usr/themes/onecircle/onecircle-dev/includes/body-layout-end.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * author:gogobody
- * time：2020-10-11 19：39
- */
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-?>
-
-</main></div></div>
--- usr/themes/onecircle/onecircle-dev/libs/language.php
+++
@@ -1,7 +0,0 @@
-<?php
-global $language;
-$language_cn = array(
-    "defaultAddr" => "云深不知处"
-);
-
-$language = $language_cn;
